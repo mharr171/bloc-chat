42266cb3d88af746056adc397c4d4453a01d5b09
Matthew Harris <mharr171.z@gmail.com>
2017-10-22 18:14:31 -0700

    Finished basic setup

---
+++ app/scripts/firebase-init.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyD0uc8BL1JiBFfW7qajgDxsCZfkowxTIkg",
+  authDomain: "bloc-chat-0213.firebaseapp.com",
+  databaseURL: "https://bloc-chat-0213.firebaseio.com",
+  projectId: "bloc-chat-0213",
+  storageBucket: "bloc-chat-0213.appspot.com",
+  messagingSenderId: "345852362588"
+};
+firebase.initializeApp(config);
